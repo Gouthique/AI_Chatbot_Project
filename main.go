f941b0f15498dfc8ceef396920356016d353bd29
gouthique <gouthamgosh10@gmail.com>
2023-11-09 22:41:59 -0500

    imported packages Progress-Task Automation

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/krognol/go-wolfram"
+	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+
+	witai "github.com/wit-ai/wit-go/v2"
 )
 
 var wolframClient *wolfram.Client
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {}
+	godotenv.Load(".env")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
